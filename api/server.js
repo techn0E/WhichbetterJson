ca3d755a9714cbfc39bfc42c5e2a8653a6f24069
techn0E <152222424+techn0E@users.noreply.github.com>
2024-08-27 03:22:32 +0400

    Update server.js

--- api/server.js
+++ api/server.js
@@ -16,6 +16,13 @@ const router = jsonServer.router('db.json')
 
 const middlewares = jsonServer.defaults()
 
+// Add custom middleware for CORS
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Allow any origin
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 server.use(middlewares)
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
